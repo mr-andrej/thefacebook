d83d3e3c143a3a9b7af46261d9e285267f2549ac
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-02-04 10:25:27 +0100

    Slight oversight. I need to add a row to the users table as a foreign_key to know if someone is a friend, not sure how I'll do it yet

--- home.php
+++ home.php
@@ -82,12 +82,31 @@ db_connect();
             <div class="panel panel-default">
                 <div class="panel-body">
                     <h4>add friend</h4>
-                    <ul>
-                        <li>
-                            <a href="#">alberte</a>
-                            <a href="#">[add]</a>
-                        </li>
-                    </ul>
+                    <?php
+                    $sql = "SELECT id, email, (SELECT COUNT(*) FROM friends WHERE friends.user_id = users.id 
+                                    AND friends.friend_id = {$_SESSION['user_id']}) AS is_friend FROM users 
+                                    WHERE id != {$_SESSION['user_id']} HAVING is_friend = 0";
+                    $result = $connection->query($sql);
+
+                    if ($result->num_rows > 0) {
+                        ?>
+                        <ul><?php
+
+                        while ($fc_user = $result->fetch_assoc()) {
+                            ?>
+                            <li>
+                            <a href="profile.php?email=<?php echo $fc_user['email']; ?>">
+                                <?php echo $fc_user['email']; ?>
+                            </a>
+                            <a href="php/add-friend.php?uid=<?php echo $fc_user['id']; ?>">[add]</a>
+                            </li><?php
+                        }
+
+                        ?></ul><?php
+                    } else {
+                        ?><p class="text-center">No users to add!</p><?php
+                    }
+                    ?>
                 </div>
             </div>
             <!-- ./add friend -->
