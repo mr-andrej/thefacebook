f0492dfeeec05234e2e52f380a982b8cb3d2b00d
Andrej Ilievski <mrandrej.ilievski@gmail.com>
2021-02-05 04:12:39 +0100

    Rendered the add friend by email fully operational, need to add a failsafe system.

--- _functions.php
+++ _functions.php
@@ -35,4 +35,3 @@ function check_auth()
 }
 
 
-
--- php/add-friend.php
+++ php/add-friend.php
@@ -8,9 +8,12 @@ $sql = "INSERT INTO friend_requests (user_id, friend_id) VALUES (?, ?)";
 // If the redirection happened from the button to the left of a person, use that
 // If the redirection happened from the email input, use the function to find the desired ID
 // TODO: Show "you're already friends" if the person is already a friend
-
-$targetID = (isset($_GET['uid'])) ? $_GET['uid'] : 1;
-
+if (isset($_GET['email'])) {
+    var_dump($_GET['email']);
+    $targetID = $connection->query("SELECT `id` FROM users WHERE email='" . $_GET['email'] . "'")->fetch_object()->id;
+    var_dump($targetID);
+} else
+    $targetID = $_GET['uid'];
 
 $statement = $connection->prepare($sql);
 $statement->bind_param('ii', $_SESSION['user_id'], $targetID);
